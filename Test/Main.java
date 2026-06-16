231f7dbe0a92e792b623946c2fdd9b2b8d0e9231
leanhappylife <159279352+leanhappylife@users.noreply.github.com>
2026-06-16 14:12:00 +0800

    Update Main.java
    是

--- Test/Main.java
+++ Test/Main.java
@@ -2,3 +2,4 @@ powershell -NoProfile -ExecutionPolicy Bypass -Command "Unblock-File -LiteralPat
 
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\db_check\db-relationship-extractor2_handoff.ps1' -OutputDir 'C:\db_check\db-relationship-extractor2' -Force"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\db_check\db-relationship-extractor2_handoff.ps1' -OutputDir 'C:\db_check\db-relationship-extractor2' -VerifyOnly"
